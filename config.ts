7871dee573cac7c4b2d08fc652448b35bdac04c8
nekrasovs <nekrasov.s@citilink.ru>
2023-12-15 20:46:00 +0800

    config for prod

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const API_URL_DEV = 'http://localhost:4000';
-export const API_URL = API_URL_DEV //'https://estate-adviser.azurewebsites.net/';
+export const API_URL = 'https://estate-adviser.azurewebsites.net/';
 
 export const GOOGLE_CLIENT_ID = '756758641886-8sggl19esej4fg44bu5jgatdaagrrc9l.apps.googleusercontent.com';
